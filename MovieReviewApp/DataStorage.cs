424702015361c38f39f5d5c667633ecbec3787d3
Kevin Corado <kevincorado97@yahoo.com>
2016-08-16 23:29:04 -0400

    Adjusted the ReadReviewPage and WriteReviewPage

--- MovieReviewApp/DataStorage.cs
+++ MovieReviewApp/DataStorage.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,10 +18,9 @@ namespace MovieReviewApp
         /// </summary>
         /// <param name="targetMovieTitle">This parameter will be used in a json query to locate all reviews with the passed movie title</param>
         /// <returns>Returns a movie review list containing all the reviews for the specified movie</returns>
-        public static List<Review> LoadData(string targetMovieTitle)
+        public static void LoadData(string targetMovieTitle)
         {
-            List<Review> reviewList = new List<Review>();
-            return reviewList;
+
         }
 
         /// <summary>
@@ -30,28 +30,16 @@ namespace MovieReviewApp
         /// <param name="CurrentReviewList">Takes a Review list as a parameter to add to a json file</param>
         public static void SaveData(Review CurrentReview)
         {
-            List<Review> _data = new List<Review>();
-            _data.Add(new Review()
-            {
-                Title = CurrentReview.Title,
-                Rate = CurrentReview.Rate,
-                Descript = CurrentReview.Descript,
-                Author = CurrentReview.Author
-            });
-            string json = JsonConvert.SerializeObject(_data.ToArray());
 
-            // Write string to file
-            System.IO.File.WriteAllText("MovieReviewDatabase.txt", json);
         }
 
         /// <summary>
         /// This will scan a json file and return a review object containing the same movie title
         /// </summary>
         /// <param name="targetMovieTitle">This parameter will be used in a json query to locate all reviews with the passed movie title</param>
-        public static Review RetriveData(string targetMovieTitle)
+        public static void RetriveData(string targetMovieTitle)
         {
-            Review review = new Review(null, null, null, null);
-            return review;
+
         }
     }
 }
--- MovieReviewApp/ReadReviewPage.xaml.cs
+++ MovieReviewApp/ReadReviewPage.xaml.cs
@@ -31,5 +31,47 @@ namespace MovieReviewApp
         {
             this.Frame.Navigate(typeof(MainPage));
         }
+
+        private void OnTarzanSelect(object sender, RoutedEventArgs e)
+        {
+            movieTitle.Content = "Tarzan";
+
+            _reviewBlock.Items.Clear();
+
+            _reviewBlock.Items.Add("-------------------\nRating: 3/5" + "\nReview: Good for it's time. Nostalgic." + "\nReviewer: John Smith\n-------------------");
+            _reviewBlock.Items.Add("-------------------\nRating: 4/5" + "\nReview: Pretty old, but pretty good." + "\nReviewer: Joe Balkans\n-------------------");
+        }
+
+        private void OnMadMaxSelect(object sender, RoutedEventArgs e)
+        {
+            movieTitle.Content = "Mad Max: Fury Road";
+
+            _reviewBlock.Items.Clear();
+
+            _reviewBlock.Items.Add("-------------------\nRating: 4/5" + "\nReview: Doesn't compare to the originals." + "\nReviewer: John Smith\n-------------------");
+            _reviewBlock.Items.Add("-------------------\nRating: 5/5" + "\nReview: Best directed movie of 2015." + "\nReviewer: Joe Balkans\n-------------------");
+            _reviewBlock.Items.Add("-------------------\nRating: 100000/5" + "\nReview: *Music plays*" + "\nReviewer: JOHN CENA\n-------------------");
+        }
+
+        private void OnNatureCallsSelect(object sender, RoutedEventArgs e)
+        {
+            movieTitle.Content = "Ace Ventura: When Nature Calls";
+
+            _reviewBlock.Items.Clear();
+
+            _reviewBlock.Items.Add("-------------------\nRating: 5/5" + "\nReview: One of the best Ace Ventura movies to date!" + "\nReviewer: John Smith\n-------------------");
+            _reviewBlock.Items.Add("-------------------\nRating: 5/5" + "\nReview: Can't get better than Jim Carrey. He's my favorite!" + "\nReviewer: Joe Balkans\n-------------------");
+        }
+
+        private void OnHolyGrailSelect(object sender, RoutedEventArgs e)
+        {
+            movieTitle.Content = "Monty Python And The Holy Grail";
+
+            _reviewBlock.Items.Clear();
+
+            _reviewBlock.Items.Add("-------------------\nRating: 5/5" + "\nReview: Pretty good for it's time." + "\nReviewer: John Smith\n-------------------");
+            _reviewBlock.Items.Add("-------------------\nRating: 5/5" + "\nReview: Was it an African Swallow?" + "\nReviewer: Joe Balkans\n-------------------");
+            _reviewBlock.Items.Add("-------------------\nRating: 0/5" + "\nReview: It was just a flesh wound." + "\nReviewer: The Black Knight\n-------------------");
+        }
     }
 }
